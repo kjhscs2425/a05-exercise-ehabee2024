51a395440a167aab8e541ad32cd725491f68c394
ehabee <ebender2025@kehillahstudent.org>
2025-01-16 23:30:41 -0800

    done

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ def check_fever(temperature):
 
 # Get temperature from user and convert to float
 temp = ...
-if check_fever(temp):
+if check_fever(100):
   print("You have a fever.")
 else:
   print("You don't have a fever.")
+
+
+
+
+
+  # Function to check if the temperature indicates a fever
+def check_fever(temp):
+    return temp >= 100.4
+
+# Get temperature from user and convert to float
+temp = float(input("Enter your temperature: "))
+
+# Check if the person has a fever
+if check_fever(temp):
+    print("You have a fever.")
+else:
+    print("You don't have a fever.")
